9ecdc5d90907120aa4dbfaa1eb37d4e61e345c94
stweiyy <stweiyy@gmail.com>
2014-11-17 15:54:20 -0500

    init structure

--- main.cpp
+++ main.cpp
@@ -1,1 +1,23 @@
+/* this is the entry file of the source, enjoy!*/
+/* @author whisly */
+
+
+
+
+
+
 #include <iostream>
+#include <string>
+/* global variables */
+std::string conf_name="";
+/* parse the command line arguments  */
+static void parse_args(int argc, char **argv);
+
+int main(int argc,char **argv){
+    
+    return 0;
+}
+void parse_args(int argc, char **argv){
+    
+    return ;
+}
